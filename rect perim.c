7c0e1b35c2fdb0fed88f571054b78fd03cf42394
har2h777-hub <har2h777@gmail.com>
2026-09-19 00:06:41 +0530

    Add files via upload

---
+++ rect perim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int length = 10;
+    int width = 5;
+    int perimeter = 2 * (length + width);
+    printf("The perimeter is: %d", perimeter);
+
+    return 0;
+}
