bb3f0ec382acb0cd8022e9e69c97625909e83f31
har2h777-hub <har2h777@gmail.com>
2026-09-19 00:26:55 +0530

    Add files via upload

---
+++ larger no.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    if (x > y) {
+        printf("%d is larger", x);
+    } else {
+        printf("%d is larger", y);
+    }
+
+    return 0;
+}
